dccfe1017464ea54b2965348fc56a21631976820
michaelparkernorton <michaelparkernorton.va@gmail.com>
2024-03-12 16:15:13 -0400

    updates

---
+++ fullstack.js
@@ -0,0 +1,26 @@
+import data from '/json/fullstack/week10.json' assert { type: 'json' };
+
+//create a new div element
+const newDiv = document.createElement("div")
+newDiv.classList.add('group')
+//create a new h1 elemnt
+const newH2 = document.createElement("h2")
+newH2.textContent = "Canvas Week 10"
+newDiv.appendChild(newH2);
+
+data.forEach(link => {
+
+	// create a new a element
+  const newLink = document.createElement("a");
+
+  // and give it some content
+	newLink.href = link.url
+	newLink.innerText = link.name
+	newLink.target = "_blank"
+	newLink.classList.add('orange')
+
+  newDiv.appendChild(newLink);
+});
+
+const main = document.querySelector("main")
+main.appendChild(newDiv)
